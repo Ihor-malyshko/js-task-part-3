52ec9a6409f6b652427ce5edd12d358f3782f269
Ihor-malyshko <malishkoio@gmail.com>
2020-07-27 22:42:36 +0300

    module 10 done

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,3 +1,21 @@
-import './menu.json';
+import menuItemsTemplate from '../templates/menu-items.hbs';
+import tagListTemplate from '../templates/tag-list.hbs';
+import itemsJSON from './menu.json';
 
-console.log('end menu');
+createMenu();
+enterTags();
+
+function createMenu() {
+  const items = menuItemsTemplate(itemsJSON);
+  const menuRef = document.querySelector('.js-menu');
+  menuRef.insertAdjacentHTML('beforeend', items);
+}
+
+function enterTags() {
+  const tagsRef = document.querySelectorAll('.tag-list');
+  const ingredients = itemsJSON.map(el => el.ingredients);
+  for (let i = 0; i < tagsRef.length; i++) {
+    const ingredient = tagListTemplate(ingredients[i]);
+    tagsRef[i].insertAdjacentHTML('beforeend', ingredient);
+  }
+}
