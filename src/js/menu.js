2c81388800f3ad5585e9345bf36ca4fd49dbc478
ihor <malishkoio@gmail.com>
2020-08-14 14:03:18 +0300

    fix

--- src/js/menu.js
+++ src/js/menu.js
@@ -3,19 +3,9 @@ import tagListTemplate from '../templates/tag-list.hbs';
 import itemsJSON from './menu.json';
 
 createMenu();
-// addTags();
 
 function createMenu() {
   const items = menuItemsTemplate(itemsJSON);
   const menuRef = document.querySelector('.js-menu');
   menuRef.insertAdjacentHTML('beforeend', items);
-}
-
-function addTags() {
-  const tagsRef = document.querySelectorAll('.tag-list');
-  const ingredients = itemsJSON.map(el => el.ingredients);
-  for (let i = 0; i < tagsRef.length; i++) {
-    const ingredient = tagListTemplate(ingredients[i]);
-    tagsRef[i].insertAdjacentHTML('beforeend', ingredient);
-  }
 }
