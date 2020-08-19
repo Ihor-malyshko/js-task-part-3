6168f529b8f5f0f989e3720af17c498448f88b81
ihor <malishkoio@gmail.com>
2020-08-19 20:31:23 +0300

    fix done

--- src/js/change-theme.js
+++ src/js/change-theme.js
@@ -8,37 +8,30 @@ const checkboxThemeRef = document.querySelector('.js-switch-input');
 const bodyRef = document.querySelector('body');
 
 initTheme();
-setTheme();
 checkboxThemeRef.addEventListener('click', chengaTheme);
 
 function initTheme() {
   if (localStorage.getItem('theme') === null) {
     theme = Theme.LIGHT;
     localStorage.setItem('theme', theme);
-  }
-}
-
-function setTheme() {
-  if (localStorage.getItem('theme') === Theme.LIGHT) {
-    theme = Theme.LIGHT;
-    checkboxThemeRef.checked = false;
-    bodyRef.classList.add(theme);
   } else {
-    theme = Theme.DARK;
-    checkboxThemeRef.checked = true;
-    bodyRef.classList.add(theme);
+    theme = localStorage.getItem('theme');
   }
+  checkboxThemeRef.checked = theme === Theme.LIGHT ? false : true;
+  bodyRef.classList.add(theme);
 }
 
 function chengaTheme() {
-  let oldTheme = theme;
+  let oldTheme = localStorage.getItem('theme');
   if (theme === Theme.LIGHT) {
     theme = Theme.DARK;
     localStorage.setItem('theme', theme);
     bodyRef.classList.replace(oldTheme, theme);
+    checkboxThemeRef.checked = true;
   } else {
     theme = Theme.LIGHT;
     localStorage.setItem('theme', theme);
     bodyRef.classList.replace(oldTheme, theme);
+    checkboxThemeRef.checked = false;
   }
 }
